d33a3ccab1558f999057d5b5cbee2d395b96530a
StephenW <steve.wiggins7@gmail.com>
2025-03-15 16:37:55 -0400

    add inertia and vue

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: Object })
+</script>
+
+<template>
+        <Head title="Welcome" />
+        <h1>Welcome</h1>
+</template>
