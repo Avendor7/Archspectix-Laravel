1320a17a0a14c008a238b1371a324ed0669e6dbc
StephenW <steve.wiggins7@gmail.com>
2025-03-16 23:08:18 -0400

    Start copying stuff from the other project

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+defineProps()
+</script>
+
+<template>
+    <Head title="Archspectix" />
+    <header>
+        <nav>
+            <a href="https://archlinux.org" class="split">Arch Linux</a>
+            <a href="https://wiki.archlinux.org/title/Main_page" class="split">Arch Wiki</a>
+            <a href="https://archlinux.org/packages/" class="split">Arch Package Search</a>
+            <a href="https://aur.archlinux.org/packages" class="split">Arch User Repository</a>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+    text-decoration: none;
+    color: var(--color-primary);
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+
+nav a:last-of-type {
+    border: 0;
+}
+
+nav a.split {
+    float: right;
+}
+</style>
