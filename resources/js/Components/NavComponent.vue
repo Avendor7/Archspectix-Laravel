8e550bed84daca6807fa8c01f1a4220c4fb261a8
StephenW <steve.wiggins7@gmail.com>
2025-03-30 18:01:38 -0400

    Move nav CSS

--- resources/js/Components/NavComponent.vue
+++ resources/js/Components/NavComponent.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
     <nav>
         <button>Button</button>
@@ -13,6 +9,28 @@
     </nav>
 </template>
 
-<style scoped>
+<script setup>
 
+</script>
+
+<style scoped>
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+    text-decoration: none;
+    color: var(--color-primary);
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+
+nav a:last-of-type {
+    border: 0;
+}
+
+nav a.split {
+    float: right;
+}
 </style>
--- resources/js/Layouts/Layout.vue
+++ resources/js/Layouts/Layout.vue
@@ -14,23 +14,5 @@ import NavComponent from "../Components/NavComponent.vue";
 </template>
 
 <style scoped>
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-    text-decoration: none;
-    color: var(--color-primary);
-}
 
-nav a:first-of-type {
-    border: 0;
-}
-
-nav a:last-of-type {
-    border: 0;
-}
-
-nav a.split {
-    float: right;
-}
 </style>
