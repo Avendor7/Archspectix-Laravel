9137bdcb33999e1afbe4a2a08f3b7b33769aeca2
StephenW <steve.wiggins7@gmail.com>
2025-05-18 20:41:38 -0400

    Refactor Home.vue to simplify structure and remove unused code
    Removed unnecessary table styles and unused imports for cleaner code. Simplified template by retaining only essential elements for layout.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,26 +1,7 @@
 <template>
-    <SearchLayout>
-        <main class="flex-col-center"></main>
-    </SearchLayout>
+    <SearchLayout> </SearchLayout>
 </template>
 
-<script setup lang="ts">
-import SearchLayout from '@/Layouts/SearchLayout.vue';
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-th,
-td {
-    border: 1px solid var(--color-primary);
-    padding: 10px;
-    text-align: left;
-}
-
-th {
-    background-color: #010101;
-}
-</style>
+<style scoped></style>
