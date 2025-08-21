aee15364934e8a1eb17ac03ba8b042a98001dfde
StephenW <steve.wiggins7@gmail.com>
2025-08-20 20:41:49 -0400

    Use new Laravel 12 starter kit base. All old functionality to be brought back gradually

--- resources/js/Pages/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <SearchLayout />
-</template>
-
-<script setup lang="ts">
-import SearchLayout from '@/Layouts/SearchLayout.vue';
-</script>
-
-<style scoped></style>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\ArchPackageController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PackageController;
+use Inertia\Inertia;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/search', [ArchPackageController::class, 'searchAll']);
-Route::get('/alr-details', [PackageController::class, 'alrDetails'])->name('alr.details');
-Route::get('/aur-details', [PackageController::class, 'aurDetails'])->name('aur.details');
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
